e2f36d10109924de7d81142559e9254a3c582bab
Guillaume Maiano (@RebelBase) <guillaume.maiano@progis.fr>
2016-07-04 01:30:19 +0200

    Quick and dirty

---
+++ source/War.java
@@ -0,0 +1,20 @@
+import Warrior
+import RallyAction
+import HealAction
+
+public class War {
+
+        public static void main(String[] args) {
+            
+            // Piotr is a warrior, who has healer training. 
+            Warrior piotr = new Warrior(new HealAction());
+            piotr.effectTroops();  // should display "Heals nearby troops (whether in sight or not)"
+
+            // oh, my... all other officers are dead. Piotr assumes command.
+            piotr.setTroopEffector(new RallyAction());
+            piotr.effectTroops(); // should display "Rally all troops in sight (whether nearby or not)"
+
+            }
+                    
+}
+
---
+++ source/Warrior.java
@@ -0,0 +1,9 @@
+import TroopEffector.java
+
+// this is the Context that will encapsulate a Strategy
+public class Warrior {
+
+    private TroopEffector troopEffector; // this is the private object that contains the Strategy
+
+
+}
